917f9cb46b88f98e67da97ba65d2ce50243a9760
Fernanda Perovano Ribeiro Montes <nandaperovanorm@gmail.com>
2022-10-06 22:06:07 -0300

    Criaçao do textField

--- notasAPP/notasAPP/Main /View/MainView.swift
+++ notasAPP/notasAPP/Main /View/MainView.swift
@@ -10,14 +10,36 @@ import UIKit
 
 class MainView: UIView {
 
+    lazy private var textField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Pesquise"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .blue
+        setupView()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        setupTextField()
+    }
+    
+    private func setupTextField() {
+        addSubview(textField)
+        NSLayoutConstraint.activate([
+            textField.topAnchor.constraint(equalTo: topAnchor),
+            textField.leadingAnchor.constraint(equalTo: leadingAnchor),
+            textField.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+        
     }
     
 }
