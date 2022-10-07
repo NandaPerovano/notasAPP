df0b37399fa97e342a0c26a59dded77db36a0f26
Fernanda Perovano Ribeiro Montes <nandaperovanorm@gmail.com>
2022-10-06 21:39:30 -0300

    Primeiro commit

---
+++ notasAPP/notasAPP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  notasAPP
+//
+//  Created by Fernanda Perovano Ribeiro Montes on 06/10/22.
+//  Copyright © 2022 teste. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
